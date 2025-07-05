e560a347e5bbe93a8df2ba0920a74726f22a61b9
Abdul Rehman <abdulrehman226721skp@gmail.com>
2025-07-05 14:32:37 +0500

    responsive

--- tailwind.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ['./build/**/*.{html,php,css,js}'], // Scans all subfolders within 'build'
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
